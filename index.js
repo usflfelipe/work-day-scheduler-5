a1914a8505ca4da8939487ca9a956b77e3b7f4b5
Felipe Ursini <usfl.felipe@gmail.com>
2023-08-22 21:18:57 -0400

    updated head

--- index.js
+++ index.js
@@ -40,5 +40,61 @@
             <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn11"></button>
 
         </div>
+        <div class='row time-block'>
+            <div class="hour col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                12am
+            </div>
+            <textarea class="future col-xs-9 col-sm-9 col-md-9 col-lg-9 description" data-hour='12' id="div12"></textarea>
+            <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn12"></button>
+
+        </div>
+        <div class='row time-block'>
+            <div class="hour col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                1pm
+            </div>
+            <textarea class="future col-xs-9 col-sm-9 col-md-9 col-lg-9 description" data-hour='13' id="div1"></textarea>
+            <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn1"></button>
+
+        </div>
+        <div class='row time-block'>
+            <div class="hour col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                2pm
+            </div>
+            <textarea class="future col-xs-9 col-sm-9 col-md-9 col-lg-9 description" data-hour='14' id="div2"></textarea>
+            <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn2"></button>
+
+        </div>
+        <div class='row time-block'>
+            <div class="hour col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                3pm
+            </div>
+            <textarea class="future col-xs-9 col-sm-9 col-md-9 col-lg-9 description" data-hour='15' id="div3"></textarea>
+            <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn3"></button>
+
+        </div>
+        <div class='row time-block'>
+            <div class="hour col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                4pm
+            </div>
+            <textarea class="future col-xs-9 col-sm-9 col-md-9 col-lg-9 description" data-hour='16' id="div4"></textarea>
+            <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn4"></button>
+
+        </div>
+        <div class='row time-block'>
+            <div class="hour col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                5pm
+            </div>
+            <textarea class="future col-xs-9 col-sm-9 col-md-9 col-lg-9 description" data-hour='17' id="div5"></textarea>
+            <button class="col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn" id="btn5"></button>
+
+        </div>
+
+    </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="./Assets/script.js"></script>
 </body>
 </html>
